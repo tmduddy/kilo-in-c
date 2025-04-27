2de4a7c6541653bbc18efdfdd7399efe8b73eb9b
tmduddy <login@tylerduddy.com>
2025-04-26 21:44:23 -0400

    replace writes with buffer appends

--- kilo.c
+++ kilo.c
@@ -257,14 +257,14 @@ void abFree(struct abuf *ab) {
 /*
  * Draw a column of ~ to distinguish rows
  */
-void editorDrawRows(void) {
+void editorDrawRows(struct abuf *ab) {
   int y;
   for (y=0; y < E.screenrows; y++) {
-    write(STDOUT_FILENO, "~", 1);
+    abAppend(ab, "~", 1);
 
     // Don't newline the last row
     if (y < E.screenrows - 1) {
-      write(STDOUT_FILENO, "\r\n", 2);
+      abAppend(ab, "\r\n", 2);
     }
   }
 }
@@ -276,11 +276,16 @@ void editorDrawRows(void) {
  *   [H - Reposition Cursor (using default arg 1 to send the cursor to col 1).
  */
 void editorRefreshScreen(void) {
-  write(STDOUT_FILENO, "\x1b[2J", 4);
-  write(STDOUT_FILENO, "\x1b[H", 3);
+  struct abuf ab = ABUF_INIT;
 
-  editorDrawRows();
-  write(STDOUT_FILENO, "\x1b[H", 3);
+  abAppend(&ab, "\x1b[2J", 4);
+  abAppend(&ab, "\x1b[H", 3);
+
+  editorDrawRows(&ab);
+  abAppend(&ab, "\x1b[H", 3);
+
+  write(STDOUT_FILENO, ab.b, ab.len);
+  abFree(&ab);
 }
 
 /*** input ***/
