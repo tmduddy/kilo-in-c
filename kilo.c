83659c92adc1a78965cb91d5c22c64ae0e6cac9c
tmduddy <login@tylerduddy.com>
2025-04-26 23:34:20 -0400

    support DEL, PG_UP, PG_DN, HOME, END

--- kilo.c
+++ kilo.c
@@ -27,7 +27,12 @@ enum editorKey {
   ARROW_LEFT = 1000,
   ARROW_RIGHT,
   ARROW_UP,
-  ARROW_DOWN
+  ARROW_DOWN,
+  DEL_KEY,
+  HOME_KEY,
+  END_KEY,
+  PAGE_UP,
+  PAGE_DOWN
 };
 
 /*** data ***/
@@ -165,12 +170,40 @@ int editorReadKey(void) {
     }
 
     // Map [A-D to our custom arrow keys
+    // Map [5,6~ to Page Up and Page Down
+    // Map [1,4,7,8 to Home and End
+    // Map OH,OF to Home and End
+    // Map [3 to DEL (not backspace)
     if (seq[0] == '[') {
+      if (seq[1] >= '0' && seq[1] <= '9') {
+        if (read(STDIN_FILENO, &seq[2], 1) != 1) {
+          return '\x1b';
+        }
+        if (seq[2] == '~') {
+          switch (seq[1]) {
+            case '1': return HOME_KEY;
+            case '3': return DEL_KEY;
+            case '4': return END_KEY;
+            case '5': return PAGE_UP;
+            case '6': return PAGE_DOWN;
+            case '7': return HOME_KEY;
+            case '8': return END_KEY;
+          }
+        }
+      } else {
+        switch (seq[1]) {
+          case 'A': return ARROW_UP;
+          case 'B': return ARROW_DOWN;
+          case 'C': return ARROW_RIGHT;
+          case 'D': return ARROW_LEFT;
+          case 'H': return HOME_KEY;
+          case 'F': return END_KEY;
+        }
+      }
+    } else if (seq[0] == 'O') {
       switch (seq[1]) {
-        case 'A': return ARROW_UP;
-        case 'B': return ARROW_DOWN;
-        case 'C': return ARROW_RIGHT;
-        case 'D': return ARROW_LEFT;
+        case 'H': return HOME_KEY;
+        case 'F': return END_KEY;
       }
     }
 
@@ -410,12 +443,28 @@ void editorProcessKeyPress(void) {
       // Exit with success code. 
       exit(0);
       break;
-  case ARROW_UP:
-  case ARROW_LEFT:
-  case ARROW_DOWN:
-  case ARROW_RIGHT:
-    editorMoveCursor(c);
-    break;
+    // Map our custom Arrow Key inputs to movement.
+    case ARROW_UP:
+    case ARROW_LEFT:
+    case ARROW_DOWN:
+    case ARROW_RIGHT:
+      editorMoveCursor(c);
+      break;
+    case HOME_KEY:
+      E.cx = 0;
+      break;
+    case END_KEY:
+      E.cx = E.screenrows - 1;
+      break;
+    case PAGE_UP:
+    case PAGE_DOWN:
+      {
+        int times = E.screenrows;
+        while (times--) {
+          editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+        }
+      }
+      break;
   }
 }
 
