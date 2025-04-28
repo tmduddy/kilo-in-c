7ebdcb81debb3b9eafeb1f86404be60e7b29fab9
tmduddy <login@tylerduddy.com>
2025-04-27 23:23:58 -0400

    support line wrap scrolling

--- kilo.c
+++ kilo.c
@@ -543,6 +543,9 @@ void editorMoveCursor(int key) {
   case ARROW_LEFT:
     if (E.cx > 0) {
       E.cx--;
+    } else if (E.cy > 0) {
+      E.cy--;
+      E.cx = E.row[E.cy].size;
     }
     break;
   case ARROW_DOWN:
@@ -553,6 +556,9 @@ void editorMoveCursor(int key) {
   case ARROW_RIGHT:
     if (row && E.cx < row->size) {
       E.cx++;
+    } else if (row && E.cx == row->size) {
+      E.cy++;
+      E.cx = 0;
     }
     break;
   case ARROW_UP:
@@ -560,6 +566,16 @@ void editorMoveCursor(int key) {
       E.cy--;
     }
     break;
+  }
+
+  // Correct the cursor's horizontal position when vertical scrolling would
+  // place the cursor in a horizontally invalid position (such as from a longer
+  // line to a shorter one)
+  row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+  int rowlen = row ? row->size : 0;
+  if (E.cx > rowlen) {
+    // If the cursor would be put in a bad spot, snap it to the end of the line
+    E.cx = rowlen;
   }
 }
 
